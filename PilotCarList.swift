117d68de21bff7241eaaa5f674a707f44bc6354b
Jump-Jay <53153234+Jump-Jay@users.noreply.github.com>
2021-06-03 11:39:15 -0500

    Add files via upload

---
+++ PilotCarList.swift
@@ -0,0 +1,17 @@
+//
+//  PilotCarList.swift
+//  Test Fires 1.0
+//
+//  Created by Jason M Davis on 6/2/21.
+//
+
+import UIKit
+
+class PilotCarList: UITableViewCell {
+
+    @IBOutlet var PilotCarImage: UIImageView!
+    @IBOutlet var homeAddress: UILabel!
+    @IBOutlet var phoneNumber: UILabel!
+    
+
+}
